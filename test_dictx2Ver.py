67cfaab3fc614f81d45eee0c51298676980efe39
Sh0rtpants <63554689+Sh0rtpants@users.noreply.github.com>
2021-01-21 18:12:27 +0000

    Netmiko-Class-1-1&2 + Sh Ver

---
+++ test_dictx2Ver.py
@@ -0,0 +1,33 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device1 = {
+    "host":'cisco3.lasthop.io',
+    "username":'pyclass',
+    "password": getpass(),
+    "device_type":'cisco_ios',
+    "session_log": 'my_session_c3.txt'
+}
+
+net_connect = ConnectHandler(**device1)
+print(net_connect.find_prompt())
+
+device2 = {
+    "host":'cisco4.lasthop.io',
+    "username":'pyclass',
+    "password": getpass(),
+    "device_type":'cisco_ios',
+    "session_log": 'my_session_c4.txt'
+}
+
+net_connect = ConnectHandler(**device2)
+print(net_connect.find_prompt())
+output1 = net_connect.send_command("sh ip int brief")
+output2 = net_connect.send_command("sh ip arp")
+output3 = net_connect.send_command("sh ver")
+print(output1)
+print(output2)
+print(output3)
+
+
+
